bcb14179b0c83c4ce4ca04137f661301718686e2
Ｎａｎｔｉｐａｔ <keymasterviriya1150@gmail.com>
2017-09-18 14:49:33 +0700

    qury data

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,11 @@ var {mongo, express} = require('./configdb');
 var router = express.Router();
 
 /* GET home page. */
+
+
 router.get('/', function(req, res, next) {
-  //res.render('index', { title: 'Express' });
+    console.log( mongo.model('people').find());
+  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
