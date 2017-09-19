e96375c4e1d3517ce2cfd09f4611d9845ae503f3
Ｎａｎｔｉｐａｔ <keymasterviriya1150@gmail.com>
2017-09-19 10:07:07 +0700

    qury all sucess

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,30 @@
-var {mongo, express} = require('./configdb');
+var {mongo, express,people} = require('./configdb');
 var router = express.Router();
 
-/* GET home page. */
 
-//mongo.model('people').find();
 
-// router.get('/', function(req, res, next) {
-//     res.send('test');
+// var query = people.find({});
+// query.select('');
+// query.exec(function (err, person) {
+//   if (err) return handleError(err);
+//   console.log(`aaa ${person[1].name}`);
 // });
-//  router.get('/test', function(req, res, next) {
-//     mongo.model('peoples').find(function(err,users)
-//      {
-//          res.send(users);
-//      });
-//  });
+
+router.get('/test', function(req, res, next) {
+
+    var query = people.find({});
+
+    query.select('name');
+
+    query.exec(function (err, person) {
+        if (err)
+            return handleError(err);
+
+        for (var i = 0, len = person.length; i < len; i++) {
+          console.log(`aaa ${person[i].name}`);
+        }
+
+    });
+});
 
 module.exports = router;
