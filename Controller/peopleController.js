02b8452bf3939d6900240a8d489b5426e8402ee3
Ｎａｎｔｉｐａｔ <keymasterviriya1150@gmail.com>
2017-10-06 09:51:51 +0700

    post man send value sucess

--- Controller/peopleController.js
+++ Controller/peopleController.js
@@ -8,6 +8,8 @@
     DELETE  /forums/:forum       ->  destroy
 */
 var {mongo, people} = require('../model/people');
+var multer  =   require('multer');
+
 exports.new = function(req, res){
 
 };
@@ -24,6 +26,25 @@ exports.index = function(req, res) {
 };
 
 exports.create = function(req, res) {
+    var storage =   multer.diskStorage({
+      destination: function (req, file, callback) {
+        callback(null, './public/images');
+      },
+      filename: function (req, file, callback) {
+        callback(null, file.fieldname + '-' + Date.now());
+      }
+    });
+
+    var upload = multer({ storage : storage}).single('img');
+    console.log(upload);
+    
+    upload(req,res, function(err) {
+        if(err) {
+            return res.end("Error uploading file.");
+        }
+        res.end("File is uploaded");
+    });
+
     console.log(req.body);
     people.collection.insert(req.body);
 };
