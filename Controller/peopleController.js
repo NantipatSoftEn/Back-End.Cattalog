2d3b7320fce33b1dba0458ff9922e218001a93e8
Ｎａｎｔｉｐａｔ <keymasterviriya1150@gmail.com>
2017-10-10 11:48:48 +0700

    add log for moniter data

--- Controller/peopleController.js
+++ Controller/peopleController.js
@@ -8,7 +8,7 @@
     DELETE  /forums/:forum       ->  destroy
 */
 var {mongo, people} = require('../model/people');
-
+var multer  =   require('multer');
 
 exports.new = function(req, res){
 
@@ -26,18 +26,38 @@ exports.index = function(req, res) {
 };
 
 exports.create = function(req, res) {
-     console.log(req.body); // For moniter Data
-     people.collection.insert(req.body);
+    var storage =   multer.diskStorage({
+      destination: function (req, file, callback) {
+        callback(null, './public/images');
+      },
+      filename: function (req, file, callback) {
+        callback(null, file.fieldname + '-' + Date.now());
+      }
+    });
+
+    var upload = multer({ storage : storage}).single('img');
+    console.log(upload);
+
+    upload(req,res, function(err) {
+        if(err) {
+            return res.end("Error uploading file.");
+        }
+        res.end("File is uploaded");
+    });
+
+    console.log(req.body);
+    people.collection.insert(req.body);
 };
 
 exports.show = function(req, res) {
-
+ console.log(req.params.c);
   var query = people.findById(req.params.c);
   query.select('');
 
   query.exec(function (err, person) {
       if (err)
           return handleError(err);
+      console.log(person);
       res.json(person);
   });
 };
